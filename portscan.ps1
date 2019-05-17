5aa12d4d9d01e058ab313a2cf17d1adca75039b1
meta-l <shrozzle@gmail.com>
2019-05-17 09:15:29 +0100

    Add files via upload
    0.6.5

--- portscan.ps1
+++ portscan.ps1
@@ -6,7 +6,9 @@ Basic port scanner to scan a single or range of IP addresses, will resolve and r
 .PARAMETER IPAddress
 Starting IP address, ending IP address. If no end supplied, only starting IP Address will be used.
 .PARAMETER Ports
-Array of target port(s)
+Array of target port(s). Required.
+.PARAMETER infile
+TExt file of IP addresses, one per line. Must either specify this option or give an IP address. Otherwise there's no point. (Mandatory false for these options so the logic works properly)
 .PARAMETER randomise
 Randomise scan of IP addresses and ports
 .PARAMETER portRange
@@ -14,15 +16,15 @@ Treat input array of target ports as a range, with input defining start and end;
 .PARAMETER resolve
 Attempt to use DNS to resolve IP address (much slower)
 .PARAMETER delay
-Apply a delay of x milliseconds before attempting next TCPClient connection
+Apply a delay of 1-1000 milliseconds before attempting next TCPClient connection
 .EXAMPLE
-.\portscan.ps1 127.0.0.1,127.0.0.4 445 (Scan port 445 on IP addresses 127.0.0.1 - 127.0.0.4)
+.\portscan.ps1 -ipaddress 127.0.0.1,127.0.0.4 -ports 445 (Scan port 445 on IP addresses 127.0.0.1 - 127.0.0.4)
 .EXAMPLE
-.\portscan.ps1 127.0.0.1 445,3389,23 -random -resolve (Scan ports 23,445,3389 on IP 127.0.0.1 randomly and resolve the IPs)
+.\portscan.ps1 -ipaddress 127.0.0.1 -ports 445,3389,23 -randomise -resolve (Scan ports 23,445,3389 on IP 127.0.0.1 randomly and resolve the IPs)
 .EXAMPLE
-.\portscan.ps1 127.0.0.1 445,450 -portRange (Scan ports 445 through 450 inclusive on 127.0.0.1)
+.\portscan.ps1 -infile .\targets.txt -portrange 445,450 (Scan ports 445 through 450 inclusive on hosts specified in targets.txt)
 .NOTES
-Version: 0.6_3
+Version: 0.6_5
 *0.1: Accept command line parameters, validate IP addresses. Thanks to @darkoperator for validation tip
 *0.2: detect single IP or scan range, iterate accordingly
 *0.3: detect single port or range, iterate accordingly
@@ -31,8 +33,9 @@ Version: 0.6_3
 *0.6: allow for range of ports (has broken numbered, single ports)
 *0.6_2: Fixed single ports
 *0.6_3: Added scan delay. Kind of a bodge until jobs are in place.
-Roadmap 0.6_4: Might be interesting to randomise delay time between 0 and given delay. Then again, it might not :P
-Roadmap 0.6_5: File of IP addresses
+*0.6_4: Might be interesting to randomise delay time between 0 and given delay. Then again, it might not :P
+*0.6_5: File of IP addresses
+Roadmap 0_6_6: Graceful errors if no IP addresses entered, rather than the red text of powershell failure
 Roadmap 0.7: rate adjustment + jobs
 Roadmap 0.8: output formatting
 Roadmap 0.9: get banners/versions
@@ -42,26 +45,38 @@ Roadmap 0.9_5: Presume could do away with port exist if statement using paramete
 [cmdletbinding()]
 
 Param(
- [Parameter(Mandatory=$true,Position = 0)][System.Net.IPAddress[]]$IPAddress
- ,[Parameter(Mandatory=$false,Position = 1)][Int[]]$ports
+ [Parameter(Mandatory=$true,Position = 0,ParameterSetName="nofile")][System.Net.IPAddress[]]$IPAddress
+ ,[Parameter(Mandatory=$true,Position = 1)][Int[]]$ports
+ ,[Parameter(Mandatory=$true,ParameterSetName="file")]$inFile
  ,[Switch]$randomise
  ,[Switch]$portRange
  ,[Switch]$resolve
- ,[Parameter(Mandatory=$false)]$delay)
+ ,[Switch]$delay)
+
+#region Initialisation
 
 $ErrorActionPreference = "SilentlyContinue"
 $DebugPreference = "Continue"
 $StartIPAddress, $EndIPAddress = $IPAddress
 $startPort, $endPort = $ports
 
-#region Initialisation Code
-#endregion Initialisation code
+#endregion Initialisation
 
 #region Process
 
-
 function makeRange {
  $ScanIPRange = @()
+ # Check for file specified on command line, populate IPAddress vars
+ if($inFile) {
+  Get-Content $inFile | ForEach-Object {
+  [system.net.ipaddress[]]$ipArrayFromFile = Get-Content $inFile
+  $StartIPAddress = $ipArrayFromFile[0]
+  Write-Debug "`$StartIPAddress is $StartIPAddress"
+  $EndIPAddress = $ipArrayFromFile[-1]
+  write-debug "`$EndIPAddress is $EndIPAddress"
+  }
+ }
+ # Generate range of IPs, or just return a single IP if only one specified on command line
  if($EndIPAddress) {
   # Many thanks to Dr. Tobias Weltner, MVP PowerShell and Grant Ward for IP range generator
   # Process Starting IP Address
@@ -85,7 +100,7 @@ function makeRange {
 return $ScanIPRange
 }
 
-
+# Populate port array if specified
 function makePortRange {
  $scanPortRange = @()
  if($portRange) {
@@ -99,7 +114,7 @@ function makePortRange {
 return $scanPortRange
 }
 
-
+# Resolve domains if specified
 function getDNS {
 $resolveArray = makeRange
 Write-Host "{*}=======Resolving=======" -ForegroundColor Magenta
@@ -114,7 +129,7 @@ Write-Host "{*}=======Resolving=======" -ForegroundColor Magenta
  Write-Host "{*}=======================" -ForegroundColor Magenta
 }
 
-
+# Standard TCP connect
 function doConnect {
  # Connects to IPs in order
  $ipLoopCount = 0
@@ -126,7 +141,7 @@ function doConnect {
   foreach($nPort in $portArray) {
    Write-Progress -Activity "Scan range $StartIPaddress - $EndIPAddress" -Status "% Complete:" -PercentComplete((($portLoopCount)/($ipArray.Length*$portArray.Length))*100)
    $portLoopCount++
-   if($delay) {Start-Sleep -m $delay}
+   if($delay) {$delay = Get-Random -maximum 1000 -Minimum 1; Start-Sleep -m $delay}
    $socket = New-Object System.Net.Sockets.TcpClient
    $socket.Connect($nIP,$nPort)
    if ($socket.Connected) {
@@ -143,7 +158,7 @@ function doConnect {
  }
 }
 
-
+# Random IP/port connect
 function doConnectRandom {
  # Randomises IP and ports arrays, iterates on IP
  write-verbose "{!} This is Gold Leader. We're starting our attack run."
@@ -154,7 +169,8 @@ function doConnectRandom {
   foreach($rPort in $rPortArray) {
    Write-Progress -Activity "Scan range $StartIPaddress - $EndIPAddress" -Status "% Complete:" -PercentComplete(($loopcount/($randomIP.Length*$rPortArray.Length))*100)
    $loopcount++
-   if($delay) {Start-Sleep -m $delay}
+   if($delay) {$delay = Get-Random -maximum 1000 -Minimum 1; Start-Sleep -m $delay}
+   write-debug "`$delay is $delay"
    $socket = New-Object System.Net.Sockets.TcpClient
    $socket.Connect($rIP,$rPort)
    if ($socket.Connected) {
@@ -164,12 +180,6 @@ function doConnectRandom {
    $socket.Close() 
   }
  }
-}
-
-
-if(-not $ports) {
- write-host "{!} No ports specified. Exiting..." -ForegroundColor Cyan
- Exit
 }
 
 
@@ -186,6 +196,6 @@ else {
 
 #endregion Process
 
-#region Finalise everything
-#endregion Finalise everything
+#region Finalise
+#endregion Finalise
 
