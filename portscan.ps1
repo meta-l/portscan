494268272b365a1da2ed9b82b049de631ba4dd6e
meta-l <shrozzle@gmail.com>
2019-06-11 11:03:50 +0100

    Add files via upload

--- portscan.ps1
+++ portscan.ps1
@@ -17,6 +17,8 @@ Treat input array of target ports as a range, with input defining start and end;
 Attempt to use DNS to resolve IP address (much slower)
 .PARAMETER delay
 Apply a delay of 1-1000 milliseconds before attempting next TCPClient connection
+.PARAMETER threads
+DEfaults of number of processors on the host running the script, can specify any number. Advise no more than 20.
 .EXAMPLE
 .\portscan.ps1 -ipaddress 127.0.0.1,127.0.0.4 -ports 445 (Scan port 445 on IP addresses 127.0.0.1 - 127.0.0.4)
 .EXAMPLE
@@ -24,7 +26,7 @@ Apply a delay of 1-1000 milliseconds before attempting next TCPClient connection
 .EXAMPLE
 .\portscan.ps1 -infile .\targets.txt -portrange 445,450 (Scan ports 445 through 450 inclusive on hosts specified in targets.txt)
 .NOTES
-Version: 0.6_5
+Version: 0.7_1
 *0.1: Accept command line parameters, validate IP addresses. Thanks to @darkoperator for validation tip
 *0.2: detect single IP or scan range, iterate accordingly
 *0.3: detect single port or range, iterate accordingly
@@ -35,11 +37,10 @@ Version: 0.6_5
 *0.6_3: Added scan delay. Kind of a bodge until jobs are in place.
 *0.6_4: Might be interesting to randomise delay time between 0 and given delay. Then again, it might not :P
 *0.6_5: File of IP addresses
-Roadmap 0_6_6: Graceful errors if no IP addresses entered, rather than the red text of powershell failure
-Roadmap 0.7: rate adjustment + jobs - jobs too intensive, will look at runspaces
+*0.7_1: Threads!
+*0.7_2: rate adjustment (specify number of threads.)
 Roadmap 0.8: output formatting
 Roadmap 0.9: get banners/versions
-Roadmap 0.9_5: Presume could do away with port exist if statement using parameter validation? Investigate
 #>
 
 [cmdletbinding()]
@@ -48,6 +49,7 @@ Param(
  [Parameter(Mandatory=$true,Position = 0,ParameterSetName="nofile")][System.Net.IPAddress[]]$IPAddress
  ,[Parameter(Mandatory=$true,Position = 1)][Int[]]$ports
  ,[Parameter(Mandatory=$true,ParameterSetName="file")]$inFile
+ ,[int]$threads
  ,[Switch]$randomise
  ,[Switch]$portRange
  ,[Switch]$resolve
@@ -60,6 +62,7 @@ $DebugPreference = "Continue"
 $StartIPAddress, $EndIPAddress = $IPAddress
 $startPort, $endPort = $ports
 
+
 #endregion Initialisation
 
 #region Process
@@ -68,13 +71,17 @@ function makeRange {
  $ScanIPRange = @()
  # Check for file specified on command line, populate IPAddress vars
  if($inFile) {
+  write-debug "`$infile is $inFile"
   Get-Content $inFile | ForEach-Object {
   [system.net.ipaddress[]]$ipArrayFromFile = Get-Content $inFile
-  $StartIPAddress = $ipArrayFromFile[0]
-  $EndIPAddress = $ipArrayFromFile[-1]
+
+  
   }
+  write-debug "`$iparrayfromfile is $ipArrayFromFile"
+  return $ipArrayFromFile  
  }
  # Generate range of IPs, or just return a single IP if only one specified on command line
+ else {
  if($EndIPAddress) {
   # Many thanks to Dr. Tobias Weltner, MVP PowerShell and Grant Ward for IP range generator
   # Process Starting IP Address
@@ -97,12 +104,14 @@ function makeRange {
  }
 return $ScanIPRange
 }
+}
+
 
 # Populate port array if specified
 function makePortRange {
  $scanPortRange = @()
  if($portRange) {
-  if($ports.Count -ge 3) {write-host "{!} Too many port arguments to calculate range. Try removing the -pR switch or specify only 2 ports. Exiting..." -ForegroundColor Cyan;Exit}
+  if($ports.Count -ge 3) {write-host "{!} Too many port arguments to calculate range. Try removing the -portrange switch or specify only 2 ports. Exiting..." -ForegroundColor Cyan;Exit}
   $scanPortRange = ($startPort..$endPort | ForEach-Object {$_})
  }
  else {
@@ -112,6 +121,7 @@ function makePortRange {
 return $scanPortRange
 }
 
+
 # Resolve domains if specified
 function getDNS {
 $resolveArray = makeRange
@@ -120,76 +130,93 @@ Write-Host "{*}=======Resolving=======" -ForegroundColor Magenta
   $hName = $null
   $hName = [System.Net.Dns]::GetHostEntry($rIP).Hostname
   if ($hName) {
-   Write-Host $rIP resolves to $hName -ForegroundColor Yellow }
+   Write-Host $rIP resolves to $hName -ForegroundColor Green }
   else {
    Write-Host $rIP does not resolve -ForegroundColor Red }
  }
  Write-Host "{*}=======================" -ForegroundColor Magenta
 }
 
+
 # Standard TCP connect
 function doConnect {
- # Connects to IPs in order
  $ipLoopCount = 0
  $portLoopCount = 0
- $ipArray = makeRange
- $portArray = makePortRange
-  foreach($nIP in $ipArray) {
-  $ipLoopCount++
-  foreach($nPort in $portArray) {
+ # check for randomise switch
+ if($randomise) {
+  $ipArray = makeRange | Sort-Object {Get-Random}
+  $portArray = makePortRange | Sort-Object {Get-Random}
+ }
+ # Connects to IPs in order
+ else {
+  $ipArray = makeRange
+  $portArray = makePortRange
+ }
+ # initialise runspaces
+ if($threads) {
+  $useThreads = $threads}
+ else {
+  $useThreads = $env:NUMBER_OF_PROCESSORS}
+ write-debug "Number of threads to use is $useThreads"
+ $infoDisplay = 
+ $pool = [RunspaceFactory]::CreateRunspacePool(1, [int]$env:NUMBER_OF_PROCESSORS + 1)
+ $pool.ApartmentState = "MTA"
+ $pool.Open()
+ $runspaces = @()
+ # set up scriptblock to pass to runspaces
+ $scriptblock = {
+  Param (
+   [ipaddress]$sb_ip
+   ,[int]$sb_port
+  )
+   # This progress bar doesn't work yet
    Write-Progress -Activity "Scan range $StartIPaddress - $EndIPAddress" -Status "% Complete:" -PercentComplete((($portLoopCount)/($ipArray.Length*$portArray.Length))*100)
-   $portLoopCount++
-   if($delay) {$delay = Get-Random -maximum 1000 -Minimum 1; Start-Sleep -m $delay}
+  if($delay) {$delay = Get-Random -maximum 1000 -Minimum 1; Start-Sleep -m $delay}
    $socket = New-Object System.Net.Sockets.TcpClient
-   $socket.Connect($nIP,$nPort)
-   if ($socket.Connected) {
-    Write-Host $nIP $nPort -ForegroundColor Green -Separator " ==> "}
- <#   $stream = $socket.GetStream()
-    Start-Sleep -m 500; $banner=""
-    while ($stream.DataAvailable) {$banner+=[char]$stream.ReadByte()}
-    write-host $banner -ForegroundColor Blue
-    Write-Debug "`$banner is $banner"#>
-   else {
-    Write-Host $nIP $nPort -ForegroundColor red -Separator " - {!} cannot open socket on port "}
+   $socket.Connect($sb_ip,$sb_Port)
+  if ($socket.Connected) {
+   #Write-Host $sb_ip $sb_Port -ForegroundColor Green -Separator " ==> "}
+   Write-Output "Connected to $sb_port on $sb_ip" }
+  else {
+   #Write-Host $sb_ip $sb_Port -ForegroundColor red -Separator " - {!} cannot open socket on port "}
+   Write-Output "Failed to connect to port $sb_port on $sb_ip" }
    $socket.Close()
   }
- }
+  
+  foreach($nIP in $ipArray) {
+   $ipLoopCount++
+   foreach($nPort in $portArray) {
+    $portLoopCount++
+    $runspace = [PowerShell]::Create()
+    $null = $runspace.AddScript($scriptblock)
+    $null = $runspace.AddArgument($nIP)
+    $null = $runspace.AddArgument($nPort)
+    $runspace.RunspacePool = $pool
+    $runspaces += [PSCustomObject]@{ Pipe = $runspace; Status = $runspace.BeginInvoke() }
+   }
+  }
+  while ($runspaces.Status -ne $null) {
+   $completed = $runspaces | Where-Object { $_.Status.IsCompleted -eq $true }
+   foreach ($runspace in $completed) {
+    $runspace.Pipe.EndInvoke($runspace.Status)
+    $runspace.Status = $null
+   }
+  } 
+
+ $pool.Close()
+ $pool.Dispose()
+
 }
 
-# Random IP/port connect
-function doConnectRandom {
- # Randomises IP and ports arrays, iterates on IP
- write-verbose "{!} This is Gold Leader. We're starting our attack run."
- $loopcount = 0
- $rPortArray = makePortRange | Sort-Object {Get-Random}
- foreach($rIP in ($randomIP = makeRange | Sort-Object {Get-Random})) {
-  #$loopcount++
-  foreach($rPort in $rPortArray) {
-   Write-Progress -Activity "Scan range $StartIPaddress - $EndIPAddress" -Status "% Complete:" -PercentComplete(($loopcount/($randomIP.Length*$rPortArray.Length))*100)
-   $loopcount++
-   if($delay) {$delay = Get-Random -maximum 1000 -Minimum 1; Start-Sleep -m $delay}
-   $socket = New-Object System.Net.Sockets.TcpClient
-   $socket.Connect($rIP,$rPort)
-   if ($socket.Connected) {
-    Write-Host $rIP $rPort -ForegroundColor Green -Separator " ==> "}
-   else {
-    Write-Host $rIP $rPort -ForegroundColor red -Separator " - {!} cannot open socket on port "}
-   $socket.Close() 
-  }
- }
-}
 
 
 Switch ($PSBoundParameters.GetEnumerator().Where({$_.Value -eq $true}).Key) {
  'resolve' {getDNS}
 }
 
-if($randomise){
- doConnectRandom
-}
-else {
- doConnect
-}
+
+doConnect
+
 
 #endregion Process
 
